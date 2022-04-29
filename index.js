ca7eb5b82274b2c6f2074ebfc68d9cb141021e84
Paul Chapdelaine <paulchapdelaine@gmail.com>
2022-04-29 14:21:04 +0200

    event a & c

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Logger = require('./emitter');
+/* const Logger = require('./emitter');
 const Logger1 = new Logger();
 
 Logger1.on("messageRecu", function(payload) {
@@ -19,4 +19,26 @@ emitter.on("messageRecu", function(payload) {
 Logger1.Log('message n°1');
 Logger1.Log('message n°2');
 Logger1.Log('message n°3');
-Logger1.Log('message n°4');
+Logger1.Log('message n°4'); */
+
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
+
+emitter.on('fizzBuzz',function(args) {
+
+    var chaine ="";
+    if(args%3==0){
+        chaine=chaine+"Fizz";
+    }
+})
+
+
+emitter.on('fizzBuzz',function(args) {
+
+    var chaine ="";
+    if(args%5==0){
+        chaine=chaine+"Buzz";
+    }
+})
+
+console.log(chaine);
